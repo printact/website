759a7f5d9e48707546564c7f6d1efdbe577e4c2c
dr-data <data@connect.hku.hk>
2021-01-24 18:06:49 +0800

    update

--- header.js
+++ header.js
@@ -61,6 +61,7 @@ document.write(`
 														</li>
 													</ul>
 												</li>
+												<!--
 												<li class="menu-item-has-children"><a href="#"><span>成人課程</span></a>
 													<ul class="sub-menu">
 														<li class="menu-item-has-children"><a href="about-us-01.html"><span>創科培訓</span></a>
@@ -77,7 +78,8 @@ document.write(`
 														</li>
 													</ul>
 												</li>
-												<li class="menu-item-has-children"><a href="#"><span>線上/到校課程</span></a>
+												-->
+												<li class="menu-item-has-children"><a href="homepage-05_curriculum.html"><span>線上/到校課程</span></a>
 													<ul class="sub-menu">
 <!-- 														<li class="menu-item-has-children"><a href="about-us-01.html"><span>About Us</span></a>
 															<ul class="sub-menu">
@@ -395,7 +397,7 @@ document.write(`
 						<div class="container">
 							<!--Menu-->
 							<ul class="sf-menu">
-								<li class="menu-item-has-children two-columns"><a href="#"><span>Home</span></a>
+								<li class="menu-item-has-children two-columns"><a href="#"><span>首頁</span></a>
 									<ul class="sub-menu">
 										<li><a href="homepage-01.html"><span>Homepage 01</span></a>
 										</li>
@@ -423,7 +425,7 @@ document.write(`
 										</li>
 									</ul>
 								</li>
-								<li class="menu-item-has-children"><a href="#"><span>About</span></a>
+								<li class="menu-item-has-children"><a href="#"><span>博士團隊</span></a>
 									<ul class="sub-menu">
 										<li class="menu-item-has-children"><a href="about-us-01.html"><span>About Us</span></a>
 											<ul class="sub-menu">
@@ -439,112 +441,226 @@ document.write(`
 										</li>
 									</ul>
 								</li>
-								<li class="menu-item-has-children"><a href="#"><span>中學IT創新實驗室</span></a>
-									<ul class="sub-menu">
-										<li class="menu-item-has-children left"><a href="portfolio-01.html"><span>Layouts</span></a>
+								<!--
+								<li class="menu-item-has-children"><a href="#"><span>成人課程</span></a>
+										<ul class="sub-menu">
+										<li class="menu-item-has-children"><a href="about-us-01.html"><span>創科培訓</span></a>
 											<ul class="sub-menu">
-												<li><a href="portfolio-01.html"><span>Portfolio 01</span></a>
+												<li><a href="about-us-01.html"><span>RTTP</span></a>
 												</li>
-												<li><a href="portfolio-02.html"><span>Portfolio 02</span></a>
+												<li><a href="about-us-02.html"><span>公司培訓</span></a>
 												</li>
-												<li><a href="portfolio-03.html"><span>Portfolio 03</span></a>
-												</li>
-												<li><a href="portfolio-04.html"><span>Portfolio 04</span></a>
+												<li><a href="about-us-03.html"><span>About Us 03</span></a>
 												</li>
 											</ul>
 										</li>
-										<li class="menu-item-has-children left"><a href="single-work-01.html"><span>Single Work</span></a>
-											<ul class="sub-menu">
-												<li><a href="single-work-01.html"><span>Style 01</span></a>
-												</li>
-												<li><a href="single-work-02.html"><span>Style 02</span></a>
-												</li>
-												<li><a href="single-work-03.html"><span>Style 03</span></a>
-												</li>
-												<li><a href="single-work-04.html"><span>Style 04</span></a>
-												</li>
-												<li><a href="single-work-05.html"><span>Style 05</span></a>
-												</li>
-												<li><a href="single-work-06.html"><span>Style 06</span></a>
-												</li>
-												<li><a href="single-work-07.html"><span>Style 07</span></a>
-												</li>
-											</ul>
+										<li><a href="about-me.html"><span>About Me</span></a>
 										</li>
 									</ul>
 								</li>
-								<li class="menu-item-has-children"><a href="#"><span>Blog</span></a>
-									<ul class="sub-menu">
-										<li class="menu-item-has-children left"><a href="blog-01.html"><span>Layouts</span></a>
-											<ul class="sub-menu">
-												<li><a href="blog-01.html"><span>Blog 01</span></a>
+								-->
+								<li class="menu-item-has-children"><a href="#"><span>線上/到校課程</span></a>
+													<ul class="sub-menu">
+<!-- 														<li class="menu-item-has-children"><a href="about-us-01.html"><span>About Us</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>About Us 01</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>About Us 02</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>About Us 03</span></a>
+																</li>
+															</ul>
+														</li> -->
+														<li><a href="about-me.html"><span>課程設計及校本課程</span></a></li>
+														<li><a href="about-me.html"><span>小學課程</span></a></li>
+														<li><a href="about-me.html"><span>中學課程</span></a></li>
+														<li><a href="about-me.html"><span>SEN課程</span></a></li>
+														<li><a href="about-me.html"><span>資優課程</span></a></li>
+
+
+													</ul>
 												</li>
-												<li><a href="blog-02.html"><span>Blog 02</span></a>
+												<li class="menu-item-has-children"><a href="#"><span>零式科學</span></a>
+												<ul class="sub-menu">
+													<li class="menu-item-has-children"><a href="about-us-01.html"><span>About Us</span></a>
+														<ul class="sub-menu">
+															<li><a href="about-us-01.html"><span>About Us 01</span></a>
+															</li>
+															<li><a href="about-us-02.html"><span>About Us 02</span></a>
+															</li>
+															<li><a href="about-us-03.html"><span>About Us 03</span></a>
+															</li>
+														</ul>
+													</li>
+													<li><a href="about-me.html"><span>About Me</span></a>
+													</li>
+												</ul>
+											</li>
+
+											<li class="menu-item-has-children"><a href="#"><span>自造者學院</span></a>
+													<ul class="sub-menu">
+														<li class="menu-item-has-children"><a href="about-us-01.html"><span>基本工程學</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>工程學原理及設計</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>電子學</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>機械學</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children"><a href="about-us-01.html"><span>數碼製造</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>電腦輔助設計(CAD)</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>電腦輔助設計(CAM)</span></a>
+																</li>
+
+															</ul>
+														</li>
+															<li class="menu-item-has-children"><a href="about-us-01.html"><span>物聯網</span></a>
+																<ul class="sub-menu">
+																	<li><a href="about-us-01.html"><span>物聯網及5G</span></a>
+																	</li>
+																	<li><a href="about-us-02.html"><span>智慧家居</span></a>
+																	</li>
+																	<li><a href="about-us-03.html"><span>智慧城市</span></a>
+																	</li>
+																</ul>
+															</li>
+															<li class="menu-item-has-children"><a href="about-us-01.html"><span>自動化</span></a>
+																<ul class="sub-menu">
+																	<li><a href="about-us-01.html"><span>自動化機器人</span></a>
+																	</li>
+																	<li><a href="about-us-02.html"><span>自動化水中機器人</span></a>
+																	</li>
+																	<li><a href="about-us-02.html"><span>工業4.0</span></a>
+																	</li>
+																</ul>
+															</li>
+														<li><a href="about-me.html"><span>About Me</span></a>
+														</li>
+													</ul>
 												</li>
-												<li><a href="blog-03.html"><span>Blog 03</span></a>
+
+												<li class="menu-item-has-children"><a href="#"><span>深科技學校</span></a>
+													<ul class="sub-menu">
+														<li class="menu-item-has-children left"><a href="about-us-01.html"><span>A - 人工智能 🤖 </span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>機器學習</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>深度學習</span></a>
+																</li>									
+																<li><a href="about-us-03.html"><span>人工智能應用</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>人工智能倫理及道德</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children left"><a href="about-us-01.html"><span>B - 區塊鏈 ⛓</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>區塊鏈及加密貨幣</span></a>
+																</li>
+																<li><a href="about-us-01.html"><span>Dapp</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>去中心化金融(DeFi)</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>金融科技(FinTech)</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children left"><a href="about-us-01.html"><span>C - 雲端運算 ☁️</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>雲端運算</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>邊緣運算</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>About Us 03</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children left"><a href="about-us-01.html"><span>D - 大數據 0️⃣1️⃣</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>數據抓取及分析</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>數據可視化</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>About Us 03</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children left"><a href="about-us-01.html"><span>深科技 🚀</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>量子計算(Quantum Computing)</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>生物信息學(Bioinformatics)</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>太空科學及科技(SpaceTech)</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children left"><a href="about-us-01.html"><span>創業家精神 💼</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>設計思維</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>商業模式設計</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>About Us 03</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children left"><a href="about-us-01.html"><span>低/無編程開發 📱</span></a>
+															<ul class="sub-menu">
+																<li><a href="about-us-01.html"><span>使用者介面/體驗設計</span></a>
+																</li>
+																<li><a href="about-us-02.html"><span>低/無編程手機程式開發</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>低/無編程網頁程式開發</span></a>
+																</li>
+																<li><a href="about-us-03.html"><span>低/無編程自動化程式開發</span></a>
+																</li>
+															</ul>
+														</li>
+														<li><a href="about-me.html"><span>About Me</span></a>
+														</li>
+													</ul>
 												</li>
-												<li><a href="blog-04.html"><span>Blog 04</span></a>
+
+												<li class="menu-item-has-children"><a href="#"><span>中學IT創新實驗室 👩‍🔬 </span></a>
+													<ul class="sub-menu">
+														<li class="menu-item-has-children left"><a href="portfolio-01.html"><span>Layouts</span></a>
+															<ul class="sub-menu">
+																<li><a href="portfolio-01.html"><span>STEM 課程</span></a>
+																</li>
+																<li><a href="portfolio-02.html"><span>STEM 產品及器材</span></a>
+																</li>
+																<li><a href="portfolio-03.html"><span>STEM Room 設計</span></a>
+																</li>
+																<li><a href="portfolio-03.html"><span>STEM 指導及配套</span></a>
+																</li>
+															</ul>
+														</li>
+														<li class="menu-item-has-children left"><a href="single-work-01.html"><span>Single Work</span></a>
+															<ul class="sub-menu">
+																<li><a href="single-work-01.html"><span>Style 01</span></a>
+																</li>
+																<li><a href="single-work-02.html"><span>Style 02</span></a>
+																</li>
+																<li><a href="single-work-03.html"><span>Style 03</span></a>
+																</li>
+																<li><a href="single-work-04.html"><span>Style 04</span></a>
+																</li>
+																<li><a href="single-work-05.html"><span>Style 05</span></a>
+																</li>
+																<li><a href="single-work-06.html"><span>Style 06</span></a>
+																</li>
+																<li><a href="single-work-07.html"><span>Style 07</span></a>
+																</li>
+															</ul>
+														</li>
+													</ul>
 												</li>
-												<li><a href="blog-05.html"><span>Blog 05</span></a>
-												</li>
-												<li><a href="blog-06.html"><span>Blog 06</span></a>
-												</li>
-												<li><a href="blog-07.html"><span>Blog 07</span></a>
-												</li>
-											</ul>
-										</li>
-										<li class="menu-item-has-children left"><a href="single-post-01.html"><span>Single Post</span></a>
-											<ul class="sub-menu">
-												<li><a href="single-post-01.html"><span>Style 01</span></a>
-												</li>
-												<li><a href="single-post-02.html"><span>Style 02</span></a>
-												</li>
-												<li><a href="single-post-03.html"><span>Style 03</span></a>
-												</li>
-												<li><a href="single-post-04.html"><span>Style 04</span></a>
-												</li>
-												<li><a href="single-post-05.html"><span>Style 05</span></a>
-												</li>
-												<li><a href="single-post-06.html"><span>Style 06</span></a>
-												</li>
-												<li><a href="single-post-07.html"><span>Style 07</span></a>
-												</li>
-											</ul>
-										</li>
-									</ul>
-								</li>
-								<li class="menu-item-has-children"><a href="#"><span>Pages</span></a>
-									<ul class="sub-menu">
-										<li class="menu-item-has-children left"><a href="services-01.html"><span>Services</span></a>
-											<ul class="sub-menu">
-												<li><a href="services-01.html"><span>Services 01</span></a>
-												</li>
-												<li><a href="services-02.html"><span>Services 02</span></a>
-												</li>
-												<li><a href="services-03.html"><span>Services 03</span></a>
-												</li>
-											</ul>
-										</li>
-										<li class="menu-item-has-children left"><a href="contact-us-01.html"><span>Contact Us</span></a>
-											<ul class="sub-menu">
-												<li><a href="contact-us-01.html"><span>Contact Us 01</span></a>
-												</li>
-												<li><a href="contact-us-02.html"><span>Contact Us 02</span></a>
-												</li>
-											</ul>
-										</li>
-										<li class="menu-item-has-children left"><a href="coming-soon-01.html"><span>Coming Soon</span></a>
-											<ul class="sub-menu">
-												<li><a href="coming-soon-01.html"><span>Coming Soon 01</span></a>
-												</li>
-												<li><a href="coming-soon-02.html"><span>Coming Soon 02</span></a>
-												</li>
-											</ul>
-										</li>
-										<li><a href="404.html"><span>Page 404</span></a>
-										</li>
-									</ul>
-								</li>
+
 <!-- 								<li class="menu-item-has-children left"><a href="#"><span>Shop</span></a>
 									<ul class="sub-menu">
 										<li class="menu-item-has-children left"><a href="shop-01.html"><span>Layouts</span></a>
